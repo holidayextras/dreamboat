e7acc823a2629f1a348d1d61ec035645ec77327a
jonathan@22acacia.com <jonathan@22acacia.com>
2015-12-13 19:40:08 +0000

    Supports multiple endpoints

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # dreamboat
-Configurable heartbeat maker
+Configurable heartbeat maker for multiple endpoints
 
 Note: This project depends on the acacia base Docker image that is currently part of headspring. Before using this repo, download headspring and create the acacia base Docker image using the command:
 
@@ -8,10 +8,10 @@ docker build -t acacia acacia/
 To Build Docker Image:
 
 1) Build docker image:
-docker build -t gcr.io/hx-test/dreamboat-initial .
+docker build -t gcr.io/hx-test/dreamboat-multi .
 
 2) Upload image to Google's image repo:
-gcloud docker push gcr.io/hx-test/dreamboat-initial
+gcloud docker push gcr.io/hx-test/dreamboat-multi
 
 To Deploy on GCP:
 
@@ -19,13 +19,16 @@ To Deploy on GCP:
 gcloud container clusters create dreamboat-container --num-nodes 1 --machine-type g1-small --scopes https://www.googleapis.com/auth/cloud-platform
 
 2) Create a pod and pass environment variables:
-kubectl run dreamboat-container --image=gcr.io/hx-test/dreamboat-initial --env="your_variable=your_value"
+kubectl run dreamboat-container --image=gcr.io/hx-test/dreamboat-multi --env="your_variable=your_value"
 
 Possible configurable env variables:
-address = address to post to (default: 127.0.0.1)
-port = port to post to (default: 8080)
-post_route = route to post to (default: /post)
+
+endpoint = comma separated list of endpoints (default: 127.0.0.1:8080/post)
+
 msg_per_sec = number of heartbeat messages to send per second (default: 2)
+
 field = field of json (default: _heartbeat_)
+
 value = value of json (default: yes)
+
 log_level = log level (default: DEBUG)
--- dreamboat/main.py
+++ dreamboat/main.py
@@ -3,62 +3,28 @@ import json
 import time
 import logging
 import ConfigParser
+import thread
 
 CONFIG_FILE = 'config'
 
-# setup logger
-logger = logging.getLogger('heartbeat_logger')
-logger.setLevel('ERROR')
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-# create formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# add formatter to ch
-ch.setFormatter(formatter)
-# add ch to logger
-logger.addHandler(ch)
+def make_logger(log_name, log_level):
+    # setup logger
+    logger = logging.getLogger(log_name)
+    logger.setLevel(log_level)
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # add formatter to ch
+    ch.setFormatter(formatter)
+    # add ch to logger
+    logger.addHandler(ch)
+    return logger
 
-def main():
-    # Load Configuration
-    config = ConfigParser.ConfigParser()
-    config.read(CONFIG_FILE)
-    
-    # ~~~ configure logger ~~~
-    # Set log level
-    LOG_LEVEL = config.get('override', 'log_level')
-    logger.setLevel(LOG_LEVEL)
-    logger.info('Using address: {0}'.format(LOG_LEVEL))
-    
-    # ~~~ configure url ~~~
-    # get address to write to
-    ADDR = config.get('override', 'address')
-    logger.info('Using address: {0}'.format(ADDR))
-    # get port
-    PORT = config.get('override', 'port')
-    logger.info('Using port: {0}'.format(PORT))
-    # get post route
-    POST = config.get('override', 'post_route')
-    logger.info('Using post route: {0}'.format(POST))
-    # create url
-    source_url = '{0}:{1}{2}'.format(ADDR,PORT,POST)
-    
-    # ~~~ get # messages per second to write ~~~
-    MSG_RATE = config.getfloat('override', 'msg_per_sec')
-    logger.info('Using message rate: {0}'.format(MSG_RATE))
-    # calculate delay between writes
-    DELAY = 1/MSG_RATE
-    
-    # ~~~ configure json payload ~~~
-    # get json field
-    FIELD = config.get('override', 'field')
-    logger.info('Using json field: {0}'.format(FIELD))
-    # get json value
-    VALUE = config.get('override', 'value')
-    logger.info('Using json value: {0}'.format(VALUE))
-    # create json
-    data = json.dumps({FIELD: VALUE})
-    
+def give_life(source_url, data, DELAY, log_level):
+    log_name = 'heartbeat_{0}'.format(source_url)
+    logger = make_logger(log_name, log_level)
     while True:
         try:
             c = pycurl.Curl()
@@ -69,14 +35,56 @@ def main():
             c.perform()
             rsp_code = c.getinfo(c.RESPONSE_CODE)
             if (rsp_code == 200):
-                logger.info('Status: %d' % rsp_code)
+                logger.info('Status from {0}: {1}'.format(source_url, rsp_code))
             else:
-                logger.error('Status: %d' % rsp_code)
+                logger.info('Status from {0}: {1}'.format(source_url, rsp_code))
             c.close()
             time.sleep(DELAY)
         except Exception as e:
             logger.error(e)
             time.sleep(5)
+
+def main():
+    # Load Configuration
+    config = ConfigParser.ConfigParser()
+    config.read(CONFIG_FILE)
+
+    # ~~~ configure logger ~~~
+    # Set log level
+    LOG_LEVEL = config.get('override', 'log_level')
+    logger = make_logger('heartbeat_main', LOG_LEVEL)
+    logger.info('Using log_level: {0}'.format(LOG_LEVEL))
+
+    # ~~~ configure url ~~~
+    # get addresses to write to
+    ADDR = config.get('override', 'endpoint')
+    logger.info('Using addresses: {0}'.format(ADDR))
+    # split addresses
+    endpoints = ADDR.split(',')
+
+    # ~~~ get # messages per second to write ~~~
+    MSG_RATE = config.getfloat('override', 'msg_per_sec')
+    logger.info('Using message rate: {0}'.format(MSG_RATE))
+    # calculate delay between writes
+    DELAY = 1/MSG_RATE
+
+    # ~~~ configure json payload ~~~
+    # get json field
+    FIELD = config.get('override', 'field')
+    logger.info('Using json field: {0}'.format(FIELD))
+    # get json value
+    VALUE = config.get('override', 'value')
+    logger.info('Using json value: {0}'.format(VALUE))
+    # create json
+    data = json.dumps({FIELD: VALUE})
+
+    # ~~~ create and spawn threads ~~~
+    for ep in endpoints:
+        logger.info('Creating pulse on {0}'.format(ep))
+        thread.start_new_thread(give_life, (ep, data, DELAY, LOG_LEVEL))
+        
+    while True:
+        pass
 
 if __name__ == '__main__':
     main()
