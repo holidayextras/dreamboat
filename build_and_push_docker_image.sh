6556226db244226f6c83766d1d33b1504117af33
pat <pat@22acacia.com>
2015-12-21 22:59:20 -0800

    initial commit for dreamboat

---
+++ build_and_push_docker_image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+/opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file account.json
+ret_var=$?
+if [ $ret_var -ne 0 ]; then
+  echo "gcloud auth command failed.  abort"
+  exit $ret_var
+fi
+
+#  push the application image to gcr
+/opt/google-cloud-sdk/bin/gcloud docker push gcr.io/hx-test/dreamboat-multi 
